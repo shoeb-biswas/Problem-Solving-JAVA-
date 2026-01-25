f972f0c6c0c549b2af30fc0e638480a3a1bd38f4
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-25 11:30:15 +0600

    1003
    Question: Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.

---
+++ Simple_Sum.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+int A,B,SOMA;
+Scanner input =new Scanner(System.in);
+A=input.nextInt();
+B=input.nextInt();
+SOMA=A+B;
+System.out.println("SOMA = "+SOMA);
+    }
+ 
+}
