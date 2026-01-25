a8f0e48bfa710ab71f89d024b923a5b6dc923380
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-25 11:27:44 +0600

    1001
    Question: Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.

---
+++ Extremely_Basic.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ public static void main(String[] args) throws IOException {
+        int A,B,X;
+        Scanner input=new Scanner (System.in);
+        A=input.nextInt();
+        B=input.nextInt();
+        X=A+B;
+ System.out.println("X = "+X);
+ 
+    }
+ 
+}
