d8eca1229557f7b51b5189e1d20e637b1e0e175d
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-26 10:46:26 +0600

    1007
    Question: Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).

---
+++ Difference.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+Scanner input= new Scanner (System.in);
+int a=input.nextInt(),b=input.nextInt(),c=input.nextInt(),d=input.nextInt(),e;
+e=((a*b)-(c*d));
+System.out.println("DIFERENCA = "+e);
+    }
+ 
+}
