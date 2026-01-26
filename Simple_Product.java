7c56ac7104f938a75bd77cd117d89e2a254b5aeb
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-26 10:43:19 +0600

    1004
    Question: Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.

---
+++ Simple_Product.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+int A,B,PROD;
+Scanner input =new Scanner(System.in);
+A=input.nextInt();
+B=input.nextInt();
+PROD=A*B;
+System.out.println("PROD = "+PROD);
+    }
+ 
+}
