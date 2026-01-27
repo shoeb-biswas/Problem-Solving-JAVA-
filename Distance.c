5cb0838f25a766945d1669fcd1f424857d97f03a
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-27 08:39:08 +0600

    1016
    Question: Two cars (X and Y) leave in the same direction. The car X leaves with a constant speed of 60 km/h and the car Y leaves with a constant speed of 90 km / h.
    
    In one hour (60 minutes) the car Y can get a distance of 30 kilometers from the X car, in other words, it can get away one kilometer for each 2 minutes.
    
    Read the distance (in km) and calculate how long it takes (in minutes) for the car Y to take this distance in relation to the other car.

---
+++ Distance.c
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+Scanner input=new Scanner (System.in);
+int a=input.nextInt();
+int b=2*a;
+System.out.printf("%d minutos\n",b);
+    }
+ 
+}
