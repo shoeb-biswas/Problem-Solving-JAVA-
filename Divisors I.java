ba1a1d8a0e3d2fd28f1ba7efeba8a7e1e5bfdd26
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-30 08:51:34 +0600

    1157
    Read an integer N and compute all its divisors.

---
+++ Divisors I.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+ Scanner s=new Scanner(System.in);
+ int N=s.nextInt();
+ for(int i=1;i<=N;i++){
+     if(N%i==0){
+         System.out.printf("%d\n",i);
+     }
+ }
+ 
+    }
+ 
+}
